84b9e8b15f09a3a6e2027ad550376e3a8829a5b0
curlywallst <nnoyes@comcast.net>
2018-01-24 12:05:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much.";
+const token = 'Your token here'
+// fetch('https://api.github.com/repos/jquery/jquery/commits')
+//   .then(res => res.json()) //shorthand for saying "give me the body of the response parsed as JSON"
+//   .then(json => console.log(json));
+
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
